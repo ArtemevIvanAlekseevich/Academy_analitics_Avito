db206394d7c26bd7598e31eee24991658300a96a
Ivan Artemev <jhnart@mail.ru>
2022-10-11 13:40:44 +0500

    issue-01_doctest

---
+++ Semester_1/Python/HW_4/issue-01_doctest/doctest_morse.py
@@ -0,0 +1,67 @@
+"""Morse Code Translator"""
+
+LETTER_TO_MORSE = {
+    'A': '.-', 'B': '-...', 'C': '-.-.',
+    'D': '-..', 'E': '.', 'F': '..-.',
+    'G': '--.', 'H': '....', 'I': '..',
+    'J': '.---', 'K': '-.-', 'L': '.-..',
+    'M': '--', 'N': '-.', 'O': '---',
+    'P': '.--.', 'Q': '--.-', 'R': '.-.',
+    'S': '...', 'T': '-', 'U': '..-',
+    'V': '...-', 'W': '.--', 'X': '-..-',
+    'Y': '-.--', 'Z': '--..', '1': '.----',
+    '2': '..---', '3': '...--', '4': '....-',
+    '5': '.....', '6': '-....', '7': '--...',
+    '8': '---..', '9': '----.', '0': '-----',
+    ', ': '--..--', '.': '.-.-.-', '?': '..--..',
+    '/': '-..-.', '-': '-....-', '(': '-.--.', ')': '-.--.-',
+    ' ': ' '
+}
+
+MORSE_TO_LETTER = {
+    morse: letter
+    for letter, morse in LETTER_TO_MORSE.items()
+}
+
+
+def encode(message: str) -> str:
+    """
+    Кодирует строку в соответсвие с таблицей азбуки Морзе
+
+    >>> print(encode('CODE IS 1234567890. DOES NOT IT WORK? USE (-/.))'))
+    -.-. --- -.. .   .. ...   .---- ..--- ...-- ....- ..... -.... --...
+    ---.. ----. ----- .-.-.-   -.. --- . ...   -. --- -   .. -   .-- ---
+    .-. -.- ..--..   ..- ... .   -.--. -....- -..-. .-.-.- -.--.- -.--.-
+    >>> encode('SOS') == '... --- ...'
+    True
+    >>> print(encode('THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG'))
+    - .... .   --.- ..- .. -.-. -.-   -... .-. --- .-- -.
+    ..-. --- -..-   .--- ..- -- .--. ...   --- ...- . .-.
+    - .... .   .-.. .- --.. -.--   -.. --- --.
+    >>> print(encode('HELLO WORLD!')) # doctest: +ELLIPSIS
+    Traceback (most recent call last):
+    ...
+    KeyError: '!'
+    """
+    encoded_signs = [
+        LETTER_TO_MORSE[letter] for letter in message
+    ]
+
+    return ' '.join(encoded_signs)
+
+
+def decode(morse_message: str) -> str:
+    """
+    Декодирует строку из азбуки Морзе в английский
+    """
+    decoded_letters = [
+        MORSE_TO_LETTER[letter] for letter in morse_message.split()
+    ]
+
+    return ''.join(decoded_letters)
+
+
+if __name__ == '__main__':
+    import doctest
+
+    doctest.testmod(verbose=True, optionflags=doctest.NORMALIZE_WHITESPACE)
